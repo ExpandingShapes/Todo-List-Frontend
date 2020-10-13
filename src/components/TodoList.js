6182d7b21915feafe988fb8295aaa3f4a893461c
Alexander <lapseofreason17@gmail.com>
2020-10-13 18:55:48 +0300

    add components

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoList = () => {
+    //const [inputText, setInputText] = useState('');
+    return (
+        <div className="todo-container">
+            <ul className="todo-list"></ul>
+        </div>
+    );
+}
+
+export default TodoList;
