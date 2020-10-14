82c012f201827abc3368cb25676dc7f4a59a2768
Alexander <lapseofreason17@gmail.com>
2020-10-14 18:49:08 +0300

    set up buttons handlers

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,55 @@
 import React from "react";
 
-const Form = ({ setInputText, todos, setTodos, inputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
   const inputTextHandler = (e) => {
     setInputText(e.target.value);
-    console.log(e.target.value);
   };
   const submitTodoHandler = (e) => {
     e.preventDefault();
     setTodos([
-      ...todos, { text: inputText, completed: false, id: Math.random() * 1000 }
-    ])
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 }
+    ]);
+    setInputText("");
   };
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
+  const completeAllHandler = (e) => {
+    e.preventDefault();
+    if (!todos.some(item => item.completed === false)) {
+      setTodos(
+        todos.map((item) => {
+          return { ...item, completed: false };
+        }));
+
+    } else {
+      setTodos(
+        todos.map((item) => {
+          return { ...item, completed: true };
+        }));
+    }
+  };
+  const removeCompletedHandler = (e) => {
+    e.preventDefault();
+    setTodos(todos.filter((item) => item.completed === false));
+  };
+
   return (
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
+      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
         </select>
       </div>
+      <button onClick={completeAllHandler}>Complete All</button>
+      <button onClick={removeCompletedHandler}>Remove Completed</button>
     </form>
   );
 };
